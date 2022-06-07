4ee8d609a650e78597220423a0491e1b82ec64b7
shepuk <paulshepherd90@live.co.uk>
2022-06-07 09:12:37 +0000

    Made navigation responsive and more user friendly for mobile devices

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,11 +1,22 @@
 function openNav() {
     let sideBarWidth = document.getElementById("mySidebar").style.width;
     
-    if (sideBarWidth !== "0px") {
-        document.getElementById("mySidebar").style.width = "0px";
-        document.getElementById("main").style.marginLeft = "0px";
-    } else if (!sideBarWidth || sideBarWidth == "0px") {
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-    }
+    if ($(window).width() < 992) {
+        if (sideBarWidth !== "0px") {
+            document.getElementById("mySidebar").style.width = "0px";
+            document.getElementById("main").style.marginLeft = "0px";
+        } else if (!sideBarWidth || sideBarWidth == "0px") {
+            document.getElementById("mySidebar").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "0px";
+        }
+     }
+     else {
+        if (sideBarWidth !== "0px") {
+            document.getElementById("mySidebar").style.width = "0px";
+            document.getElementById("main").style.marginLeft = "0px";
+        } else if (!sideBarWidth || sideBarWidth == "0px") {
+            document.getElementById("mySidebar").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+        }
+     }
 }
