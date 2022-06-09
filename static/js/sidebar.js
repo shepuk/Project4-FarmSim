451f31597d0e647fecc7cec689e946950134c0cb
shepuk <paulshepherd90@live.co.uk>
2022-06-09 16:06:43 +0000

    updated sidebar javascript for more consistent control, especially on mobile

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,22 +1,24 @@
 function openNav() {
-    let sideBarWidth = document.getElementById("mySidebar").style.width;
+    let sideBarWidth = document.getElementById("mySidebar").clientWidth;
+    let sideBar = document.getElementById("mySidebar");
+    let main = document.getElementById("main")
     
     if ($(window).width() < 992) {
-        if (sideBarWidth !== "0px") {
-            document.getElementById("mySidebar").style.width = "0px";
-            document.getElementById("main").style.marginLeft = "0px";
-        } else if (!sideBarWidth || sideBarWidth == "0px") {
-            document.getElementById("mySidebar").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "0px";
+        if (sideBarWidth == 0) {
+            sideBar.style.width = "250px";
+            main.style.marginLeft = "0px";
+        } else if (sideBarWidth != 0) {
+            sideBar.style.width = "0px";
+            main.style.marginLeft = "0px";
         }
      }
      else {
-        if (sideBarWidth !== "0px") {
-            document.getElementById("mySidebar").style.width = "0px";
-            document.getElementById("main").style.marginLeft = "0px";
-        } else if (!sideBarWidth || sideBarWidth == "0px") {
-            document.getElementById("mySidebar").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "250px";
+        if (sideBarWidth !== 0) {
+            sideBar.style.width = "0px";
+            main.style.marginLeft = "0px";
+        } else if (!sideBarWidth || sideBarWidth == 0) {
+            sideBar.style.width = "250px";
+            main.style.marginLeft = "250px";
         }
      }
 }
