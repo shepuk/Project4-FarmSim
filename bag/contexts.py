aa15153abaa80015b8209300a9a02b1e03b10c75
shepuk <paulshepherd90@live.co.uk>
2022-06-12 10:28:25 +0000

    Updated shopping bag page to display session bag items

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from store.models import Product
 
 
 def bag_contents(request):
@@ -6,6 +8,17 @@ def bag_contents(request):
     bag_items = []
     total = 999
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     # Use code here to also calculate premium membership discount
 
