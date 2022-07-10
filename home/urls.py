23220af7074d2a1fb23a190fc3be749cc7758896
shepuk <paulshepherd90@live.co.uk>
2022-07-10 14:52:25 +0000

    Help page update and minor styling to homepage icons

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('help/', views.help, name='help'),
+    path('about/', views.about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index (request):
     return render (request, 'home/index.html')
+
+def help (request):
+    return render (request, 'home/help.html')
+
+def about (request):
+    return render (request, 'home/about.html')
